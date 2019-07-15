93f6bd4871da8ab9d809a55b887b5fbed669854f
Jesse Aldridge <jessealdridge@gmail.com>
2019-07-15 13:51:30 -0700

    comments

--- search_diagrams.js
+++ search_diagrams.js
@@ -7,6 +7,8 @@ const expand_home_dir = require('expand-home-dir');
 const inflated_dir_path = expand_home_dir('~/inflated_diagrams/');
 const inflated_paths = glob.sync(inflated_dir_path + '**/*.txt');
 const query_regex = new RegExp(process.argv[2], 'i');
+
+// Search content
 inflated_paths.forEach(function(path) {
   const content = fs.readFileSync(path, 'utf8');
   if(content.match(query_regex)) {
@@ -16,8 +18,9 @@ inflated_paths.forEach(function(path) {
   }
 });
 
+// Search paths
 inflated_paths.forEach(function(path) {
-  console.log('path:', path);
+  // console.log('comparing against path:', path);
   if(path.match(query_regex)) {
     let new_path = path.replace(/\.txt$/, '.drawio');
     new_path = new_path.replace(/\/inflated_diagrams\//, '/Dropbox/diagrams/');
