272f1200350765fe2aa506315676777dee0bb190
Jesse Aldridge <jessealdridge@gmail.com>
2019-07-15 17:00:57 -0700

    sort

--- search_diagrams.js
+++ search_diagrams.js
@@ -9,16 +9,27 @@ const inflated_paths = glob.sync(inflated_dir_path + '**/*.txt');
 const query_regex = new RegExp(process.argv[2], 'gi');
 
 // Search content
+const content_matches = [];
 inflated_paths.forEach(function(path) {
   const content = fs.readFileSync(path, 'utf8');
   const matches = content.match(query_regex);
   if(matches) {
     let new_path = path.replace(/\.txt$/, '.drawio');
     new_path = new_path.replace(/\/inflated_diagrams\//, '/Dropbox/diagrams/');
-
-    console.log(new_path, matches.length);
+    content_matches.push({
+      path: new_path,
+      count: matches.length
+    });
   }
 });
+
+content_matches.sort(function(a,b) {
+  return a.count - b.count
+});
+
+content_matches.forEach(function(match) {
+  console.log('match:', match.path, match.count);
+})
 
 // Search paths
 inflated_paths.forEach(function(path) {
