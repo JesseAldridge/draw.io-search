60d55939b6478a96c6c44a9d8da4cf0b7b787f2c
Jesse Aldridge <jessealdridge@gmail.com>
2019-09-07 21:23:24 -0700

    tfidf

--- search_diagrams.js
+++ search_diagrams.js
@@ -8,49 +8,57 @@ const inflated_dir_path = expand_home_dir('~/inflated_diagrams/');
 const inflated_paths = glob.sync(inflated_dir_path + '**/*.txt');
 
 const query_terms = process.argv.slice(2);
-console.log('query_terms:', query_terms);
+for(let i = 0; i < query_terms.length; i++)
+  query_terms[i] = query_terms[i].toLowerCase();
 
 // Search content
-const content_matches = [];
+const matches = [];
+const term_to_document_frequency = {};
 inflated_paths.forEach(function(path) {
-  const content = fs.readFileSync(path, 'utf8');
-  const term_to_match_count = {};
-  let total_match_count = 0;
+  const content = fs.readFileSync(path, 'utf8').toLowerCase();
+
+  const term_to_score = {};
+  let sum_term_score = 0;
   query_terms.forEach(function(term) {
-    term_to_match_count[term] = content.split(term).length - 1;
-    total_match_count += term_to_match_count[term];
+
+    let path_score = 0;
+    const relavent_part_of_path = path.split(inflated_dir_path)[1];
+    if(relavent_part_of_path.toLowerCase().indexOf(term) != -1)
+      path_score = 1;
+
+    const term_frequency = content.split(term).length - 1;
+    if(term_frequency > 0) {
+      if(!term_to_document_frequency[term])
+        term_to_document_frequency[term] = 0;
+      term_to_document_frequency[term] += 1;
+    }
+
+    const term_score = term_frequency + path_score * 10;
+    term_to_score[term] = term_score;
+    sum_term_score += term_score;
   });
 
-  if(total_match_count > 0) {
+  if(sum_term_score > 0) {
     let new_path = path.replace(/\.txt$/, '.drawio');
     new_path = new_path.replace(/\/inflated_diagrams\//, '/Dropbox/diagrams/');
-    content_matches.push({
+    matches.push({
       path: new_path,
-      count: total_match_count,
+      term_to_score: term_to_score,
     });
   }
 });
 
-content_matches.sort(function(a,b) {
-  return a.count - b.count
+function score(match) {
+  let document_score = 0;
+  for(let term in match.term_to_score)
+    document_score += match.term_to_score[term] / (term_to_document_frequency[term] || 1);
+  return document_score;
+}
+
+matches.sort(function(a,b) {
+  return score(a) - score(b)
 });
 
-content_matches.forEach(function(match) {
-  console.log('match:', match.path, match.count);
-})
-
-// Search paths
-inflated_paths.forEach(function(path) {
-  path = path.split(inflated_dir_path)[1];
-  for(let i = 0; i < query_terms.length; i++) {
-    const term = query_terms[i];
-
-    if(path.match(term)) {
-      let new_path = path.replace(/\.txt$/, '.drawio');
-      new_path = [inflated_dir_path, new_path].join('/');
-      new_path = new_path.replace(/\/inflated_diagrams\//, '/Dropbox/diagrams/');
-      console.log(new_path);
-      break;
-    }
-  }
+matches.forEach(function(match) {
+  console.log('match:', match.path, score(match).toFixed(2));
 });
