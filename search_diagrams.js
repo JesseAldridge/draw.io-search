5d86460bec03053f14a1cd2f37f652380fbeabcd
Jesse Aldridge <jessealdridge@gmail.com>
2019-11-23 16:53:11 -0800

    it works

--- search_diagrams.js
+++ search_diagrams.js
@@ -8,64 +8,54 @@ const cheerio = require('cheerio')
 
 
 const inflated_dir_path = expand_home_dir('~/inflated_diagrams/');
-const inflated_paths = glob.sync(inflated_dir_path + '**/*.xml');
+const inflated_paths = glob.sync(inflated_dir_path + '**/*.json');
 
 const query_terms = process.argv.slice(2);
 for(let i = 0; i < query_terms.length; i++)
   query_terms[i] = query_terms[i].toLowerCase();
 const query_string = query_terms.join(' ');
 
-// Build index
-const path_to_diagrams_to_cells = {}
-inflated_paths.forEach(function(path_) {
-  console.log('building index:', path_)
-  path_to_diagrams_to_cells[path_] = {}
-
-  const content_xml = fs.readFileSync(path_, 'utf8');
-  const graph_models = cheerio.load(content_xml);
-  graph_models('mxGraphModel').each(function(diagram_index) {
-    path_to_diagrams_to_cells[diagram_index] = {}
-
-    let diagram_elem = cheerio(this);
-    diagram_elem.find('mxCell').each(function(cell_index) {
-      path_to_diagrams_to_cells[diagram_index][cell_index] = cheerio(this);
-    })
-  })
-})
-
 // Search content
 const matches = [];
 const term_to_document_frequency = {};
 inflated_paths.forEach(function(path_) {
+  // diagram refers to a tab within a .drawio file
+  const diagrams_json = fs.readFileSync(path_, 'utf8');
+  const name_to_diagram = JSON.parse(diagrams_json)
+
   let exact_match = false;
   const filename = path.basename(path_);
   console.log('filename:', filename.toLowerCase());
-  if(filename.toLowerCase() == query_string + '.xml')
+  if(filename.toLowerCase() == query_string + '.json')
     exact_match = true;
 
-  const content_xml = fs.readFileSync(path_, 'utf8');
-
   let sum_term_score = 0;
+  const term_to_tab_to_matching_cells = {};
   const term_to_score = {};
   query_terms.forEach(function(term) {
+    let path_score = 0 // in the term in the path of the .drawio file
+    let term_frequency = 0 // the number of times the term occurs in the .drawio file
+    const tab_to_matching_cells = term_to_tab_to_matching_cells[term] = {}
 
-    let path_score = 0;
     const relavent_part_of_path = path_.split(inflated_dir_path)[1];
     if(relavent_part_of_path.toLowerCase().indexOf(term) != -1)
       path_score = 1;
 
-    let term_frequency = 0;
-
-    Object.keys(path_to_diagrams_to_cells).forEach(function(path) {
-      const diagrams = path_to_diagrams_to_cells[path];
-      diagrams.forEach(function(diagram) {
-        diagram.forEach(function(cell) {
-          const content = cell_elem.attr('value')
-
-          if(!content)
-            return
-          term_frequency += content.split(term).length - 1;
-        })
+    const tab_names = Object.keys(name_to_diagram)
+    tab_names.forEach(function(tab_name) {
+      tab = name_to_diagram[tab_name]
+      tab.cells.forEach(function(cell) {
+        const cell_match_count = cell.text.split(term).length - 1
+        term_frequency += cell_match_count;
+        if(cell_match_count > 0) {
+          if(!tab_to_matching_cells[tab_name])
+            tab_to_matching_cells[tab_name] = []
+          tab_to_matching_cells[tab_name].push({
+            x: cell.x,
+            y: cell.y,
+            excerpt: cell.text.substr(0, 10)
+          })
+        }
       })
     })
 
@@ -85,6 +75,7 @@ inflated_paths.forEach(function(path_) {
     matches.push({
       path: new_path,
       term_to_score: term_to_score,
+      term_to_tab_to_matching_cells: term_to_tab_to_matching_cells,
       exact_match: exact_match,
     });
   }
@@ -104,5 +95,6 @@ matches.sort(function(a,b) {
 });
 
 matches.forEach(function(match) {
-  console.log('match:', match.path, score(match).toFixed(2), match.term_to_score);
+  const cell_match_json = JSON.stringify(match.term_to_tab_to_matching_cells, null, 2)
+  console.log('match:', match.path, score(match).toFixed(2), cell_match_json);
 });
