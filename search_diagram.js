4b2b076210835de6e1151010a0f81528d25e30b2
Jesse Aldridge <jessealdridge@gmail.com>
2019-11-24 20:32:50 -0800

    stem

---
+++ search_diagram.js
@@ -0,0 +1,85 @@
+const fs = require('fs');
+const path = require('path');
+
+const stem_words = require('./stem_words.js');
+
+function search_diagram(inflated_dir_path, path_, query_terms, term_to_document_frequency) {
+  // diagram refers to a tab within a .drawio file
+  const diagrams_json = fs.readFileSync(path_, 'utf8');
+  const name_to_diagram = JSON.parse(diagrams_json)
+
+  // e.g. data-chop/data_chop.json
+  const relavent_part_of_path = inflated_dir_path ? path_.split(inflated_dir_path)[1] : path_;
+  const path_split = relavent_part_of_path.split('/')
+
+  const path_tokens = []
+  for(let i = 0; i < path_split.length; i++)
+    stem_words.stem_words(path_split[i]).forEach(function(token) {
+      path_tokens.push(token)
+    })
+
+  let sum_term_score = 0;
+  const term_to_tab_to_matching_cells = {};
+  const term_to_score = {};
+  query_terms.forEach(function(query_term) {
+    let path_score = 0 // is the term in the path of the .drawio file
+    path_tokens.forEach(function(path_token) {
+      if(path_token == query_term)
+        path_score += 1
+    })
+
+    let term_frequency = 0 // the number of times the term occurs in the .drawio file
+    const tab_to_matching_cells = {}
+
+    const tab_names = Object.keys(name_to_diagram)
+    tab_names.forEach(function(tab_name) {
+      tab = name_to_diagram[tab_name]
+      tab.cells.forEach(function(cell) {
+        // cell.text
+
+        const cell_tokens = stem_words.stem_words(cell.text)
+        let cell_match_count = 0
+        cell_tokens.forEach(function(cell_token) {
+          if(cell_token == query_term)
+            cell_match_count += 1
+        })
+        term_frequency += cell_match_count
+        if(cell_match_count > 0) {
+          if(!tab_to_matching_cells[tab_name])
+            tab_to_matching_cells[tab_name] = []
+          tab_to_matching_cells[tab_name].push(cell.text.substr(0, 80))
+        }
+      })
+    })
+
+    if(term_frequency > 0) {
+      if(!term_to_document_frequency[query_term])
+        term_to_document_frequency[query_term] = 0;
+      term_to_document_frequency[query_term] += 1;
+      term_to_tab_to_matching_cells[query_term] = tab_to_matching_cells
+    }
+
+    const term_score = term_frequency + path_score * 80;
+    term_to_score[query_term] = term_score;
+    sum_term_score += term_score;
+  });
+
+  if(sum_term_score > 0) {
+    let new_path = path_.replace(/\.json$/, '.drawio');
+    new_path = new_path.replace(/\/inflated_diagrams\//, '/Dropbox/diagrams/');
+    return {
+      path: new_path,
+      term_to_score: term_to_score,
+      term_to_tab_to_matching_cells: term_to_tab_to_matching_cells,
+    };
+  }
+
+  return null
+}
+
+exports.search_diagram = search_diagram
+
+if(require.main === module) {
+  const match = search_diagram(null, 'data/test-inflated.json', ['per', 'bar'], {})
+  console.log(JSON.stringify(match, null, 2))
+}
---
+++ stem_words.js
@@ -0,0 +1,36 @@
+const stemmer = require('./PorterStemmer1980.js')
+
+function tokenize(text) {
+  const re = /([A-Za-z']+)/g
+  let m = null;
+  const tokens = []
+  do {
+    m = re.exec(text)
+    if(m)
+      tokens.push(m[0].toLowerCase())
+  } while(m)
+  return tokens
+}
+
+const stem_words = (function() {
+  const word_to_stem = {}
+
+  return function(text) {
+    const tokens = tokenize(text)
+    const stemmed_words = []
+    for(let i = 0; i < tokens.length; i++) {
+      if(!word_to_stem[tokens[i]]) {
+        word_to_stem[tokens[i]] = stemmer.stemmer(tokens[i])
+      }
+      stemmed_words.push(word_to_stem[tokens[i]])
+    }
+
+    return stemmed_words
+  }
+})()
+
+exports.stem_words = stem_words
+
+if(require.main === module) {
+  console.log(stem_words('data-cats/data_cats.json'))
+}
